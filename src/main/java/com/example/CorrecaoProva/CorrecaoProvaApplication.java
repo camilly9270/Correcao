d7e2be609216707a8e2455740833145070897a08
camilly9270 <camilly.ccn@gmail.com>
2022-11-18 19:04:28 -0300

    first commit

---
+++ src/main/java/com/example/CorrecaoProva/CorrecaoProvaApplication.java
@@ -0,0 +1,13 @@
+package com.example.CorrecaoProva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrecaoProvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrecaoProvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CorrecaoProva/CorrecaoProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CorrecaoProva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrecaoProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
