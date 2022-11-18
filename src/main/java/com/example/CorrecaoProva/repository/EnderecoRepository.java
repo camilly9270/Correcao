388ea26b4098ac3a6f63b69b0479083cfec75c18
camilly9270 <camilly.ccn@gmail.com>
2022-11-18 19:42:53 -0300

    Primeiro commit

---
+++ src/main/java/com/example/CorrecaoProva/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.example.CorrecaoProva.repository;
+
+import com.example.CorrecaoProva.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
